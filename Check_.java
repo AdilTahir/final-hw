b3085b6e74a20e00cf936d5a0bb13864b3125ce7
AdilTahir <adilthebigboss@gmail.com>
2017-07-27 09:23:44 -0600

    Add files via upload

--- Check_.java
+++ Check_.java
@@ -1,40 +1,41 @@
-package checkBookProject;
-
 public class Check_ {
 
-	public static double checkAmount = 0.0; //amount the check is for
-	public static int checkNumber; //to hold the check number 
-	public static String checkPayee =  null; 
-	public static String checkReason =  null;
-	public static String checkPayer =  null;
-	
+   public double checkAmount = 0.0; //amount the check is for
+   public int checkNumber; //to hold the check number
+   public String checkPayee =  null;
+   public String checkReason =  null;
+   public String checkPayer =  null;
+   
     
     public Check_(double amount, String reason, String payee){
        //initialize the check's values
-       Check_.checkAmount= amount; 
-       Check_.checkNumber = 0;
-       Check_.checkPayee = payee;
-       Check_.checkReason = reason;
-       Check_.checkPayer = "Adil Tahir";// i know its overcoded, left this hardcode in but could swap for variable later
+       checkAmount= amount;
+       checkNumber = 0;
+       checkPayee = payee;
+       checkReason = reason;
+       checkPayer = "Adil Tahir";// i know its overcoded, left this hardcode in but could swap for variable later
        
        
-	}//Check_()
+   }//Check_()
     
-    public static void setAmount(Double inc){
-    	checkAmount = inc;
+    public void setAmount(Double inc){
+        this.checkAmount = inc;
     }
     
-    public static void setPayee (String inc){
-    	checkPayee = inc;
+    public  void setPayee (String inc){
+       this.checkPayee = inc;
     }
-    public static void setReason (String inc){
-    	checkReason = inc;
+    public  void setReason (String inc){
+       this.checkReason = inc;
     }
     
-    public static void printCheck(){
-    	System.out.println(Check_.checkPayer + "    " + Check_.checkNumber);
-        System.out.println("Pay to the order of: " + Check_.checkPayee + "     " + Check_.checkAmount);
-        System.out.println("For: " + Check_.checkReason);
+    public  void printCheck(){
+       System.out.println(this.checkPayer + "    " + this.checkNumber);
+        System.out.println("Pay to the order of: " + this.checkPayee + "     " + this.checkAmount);
+        System.out.println("For: " + this.checkReason);
     }//printCheck()
-    
+    public String toString(){
+        String result = this.checkPayer + "    " + "Pay to the order of: " + this.checkPayee + "     " + this.checkAmount;
+        return result;
+    }
 }//Class Check_
